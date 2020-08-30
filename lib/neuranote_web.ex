207a6daba893bc1542312e5fe37e1fa901d3c9ba
Dan Day <danday@gmail.com>
2020-08-30 12:21:13 -0500

    mix phx.gen.live Accounts User users username:string password_hash:string

--- lib/neuranote_web.ex
+++ lib/neuranote_web.ex
@@ -83,6 +83,7 @@ defmodule NeuranoteWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
+      import NeuranoteWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
